124159464c6470a77f2f738f3281a9ef42407af1
williamin18 <ziyuan.wang@mail.mcgill.ca>
2024-12-05 16:48:29 -0500

    .

--- PC/genPolynomialSamplesTensor.m
+++ PC/genPolynomialSamplesTensor.m
@@ -1,15 +1,22 @@
 function [y] = genPolynomialSamplesTensor(samples,order,polynomial)
 %GENHERMITE Summary of this function goes here
 %   Detailed explanation goes here
+
+
 switch polynomial
     case "Hermite"
-        f = 1;
+        f = @genHermite;
     case "Legendre"
-        f = 2;
+        f = @genLegendre;
     otherwise
         err('Unsupported polynomial type')
 end
-
+[n,d]=size(samples);
+samples = samples';
+y = zeros(d,order,n);
+for i = 0:order
+    y(:,i+1,:) = f(samples,i);
+end
 
 end
 
---
+++ TT/TT_Riemannian_GD_update.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = TT_Riemannian_GD_update(g,step_size)
+%TT_RIEMANNIAN_UPDATE Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
