03eb2a8ffe3bd893ef23fe8518a2db5fb429444d
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-03-27 00:18:06 -0400

    Update TT_Riemannian_Newton.m

--- TT/TT_Riemannian_Newton.m
+++ TT/TT_Riemannian_Newton.m
@@ -12,6 +12,9 @@ Y_yr = kron( kron(yr,           ones(1,m)), ones(1,r_k));
 Y = Y_yl.*Y_Ai.*Y_yr;
 
 
+X = kron(eye(r_k1),Xk');
+lambda =  (X/(Y'*Y)*X) \ (X/(Y'*Y)*Y*residual);
+dXk = (Y'*Y)\(A'*residual-X'*lambda);
 
 
 dXk = reshape(dXk,[r_k*m, r_k1]);
