1f0d91d39fcd3f7357bc03fdc7763557d299af14
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-04-21 19:52:06 -0400

    Completion works, but not for negative inputs

--- TT/TT_Riemannian_completion_Gradient2.m
+++ TT/TT_Riemannian_completion_Gradient2.m
@@ -1,4 +1,4 @@
-function [V,dUx,dx_TT] = TT_Riemannian_completion_Gradient(A,U,residual,beta,dx_old,lambda)
+function [V,dUx,dx_TT] = TT_Riemannian_completion_Gradient2(A,U,residual,beta,dx_old,lambda)
 %TT_NEWTON_GRADIENT Summary of this function goes here
 %   Detailed explanation goes here
 [n_samples,~] = size(A{1});
@@ -32,6 +32,9 @@ for i = 1:d
 end
 for i = 1:d-1
     dUx{i} = dUx{i} - U{i}*U{i}'*dUx{i};
+%      L = U{i}'*dUx{i};
+%     dUx{i} = dUx{i} - U{i}*L;
+%     dUx{i+1} = dUx{i+1} + h2v(L*v2h(V{i+1},m(i+1)),m(i+1));
 end
 
 if beta>0
@@ -43,12 +46,27 @@ end
 
 dx_TT = TT_Riemannian_fromGTensor(U,V,dUx);
 
-Adx = multi_r1_times_TT(A,dx_TT);
+
+Adx = zeros(n_samples,d);
+for i = 1:d
+    dUi = reshape(dUx{i},[r(i), m(i), r(i+1)]);
+    dUi = reshape(permute(dUi, [2 1 3]),m(i),[]);
+    AdU = A{i}*dUi;
+    AdU = reshape(AdU,n_samples,r(i),r(i+1));
+
+    Adxi = zeros(n_samples,r(i+1));
+    for j = 1:r(i+1)
+        Adxi(:,j) = sum(yl{i}.*AdU(:,:,j),2);
+    end
+    Adx(:,i) = sum(Adxi.*yr{i},2);
+end
+
 alpha = Adx\residual;
+%alpha = Adx\residual;
 %alpha = lsqnonneg(Adx,residual);
 
 for i = 1:d
-    dUx{i} = alpha*dUx{i};
+    dUx{i} = alpha(i)*dUx{i};
 end
 end
 
--- Tests/radTest/rand_completion_test.m
+++ Tests/radTest/rand_completion_test.m
@@ -2,12 +2,15 @@ n = 4;
 d = 10;
 N = n*ones(d,1);
 r = 3;
-n_samples = 2000;
+n_samples = 3600;
 n_test_samples = 100;
 
 
 
-X_true = TTrand(N,3);
+x_true = TTrand(N,3);
+x_true = TTorthogonalizeLR(x_true);
+x_true{d} = x_true{d}/norm( x_true{d},'fro');
+
 A = cell(d,1);
 A_test = cell(d,1);
 for i =1: d
@@ -25,12 +28,26 @@ for i =1: d
 end
 
 
-
-x_samples = multi_r1_times_TT(A,X_true);
-x_test_sample =  multi_r1_times_TT(A_test,X_true);
-
-x = TTrand(N,r);
+x = TTrand(N,3);
+x = TTorthogonalizeLR(x);
+x{d} = x{d}/norm( x{d},'fro');
 
 
-x = TT_Riemannian_completion(A,x_samples,x,r,1e-4,2000,A_test,x_test_sample,0);
-TTnorm(TTaxby(1,x,-1,X_true))/TTnorm(X_true)
+
+
+% comparsion_samples = load('Completion_comparsion_samples.mat');
+A = comparsion_samples.A;
+A_test = comparsion_samples.A_test;
+x_true = comparsion_samples.x_true;
+% x = comparsion_samples.x;
+
+
+x_sample = multi_r1_times_TT(A,x_true);
+x_test_sample =  multi_r1_times_TT(A_test,x_true);
+
+
+
+% load('Completion_comparsion_samples.mat');
+
+[x,training_err,test_err,epoch] = TT_Riemannian_completion(A,x_sample,x,r,1e-4,2000,A_test,x_test_sample,0);
+TTnorm(TTaxby(1,x,-1,x_true))/TTnorm(x_true)
