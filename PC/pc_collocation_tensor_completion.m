76c1a73c1a435547c18fc416b107bcb962f5a83c
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-05-02 17:02:18 -0400

    completion coding

--- PC/pc_collocation_tensor_completion.m
+++ PC/pc_collocation_tensor_completion.m
@@ -1,7 +1,7 @@
 function [b_predict,PC_coefficients,training_err,test_err] = ...
-    pc_collocation_tensor_completion(sample_indices,b_train,x,A_predict,order,polynomial,method)
-%PC_COLLOCATION_TENSOR Summary of this function goes here
-%   Detailed explanation goes here
+    pc_collocation_tensor_completion(sample_indices,b_train,y,A_predict,order,polynomial,method)
+%y is TT-estimation of outputs, b is sample outputs,sample_indices are indices of b,
+% A is polynomial samples(not used on training)
 switch method
     case "TT-Riemannian"
         f = @TT_Riemannian_completion;
@@ -10,9 +10,8 @@ switch method
 end
     
 
-
+%% initialization
 [n_samples,d] = size(sample_indices);
-
 
 
 n_train = round(0.9*n_samples);
@@ -21,29 +20,59 @@ training_samples = b_train(1:n_train,:);
 test_indices = sample_indices(n_train+1:end,:);
 test_samples = b_train(n_train+1:end,:);
 
-%preconditioning
+
+training_sample_selector = cell(d,1);
+test_sample_selector = cell(d,1);
 for i = 1:d
-    for j = 1:order
-        training_samples{i}(:,j+1) = training_samples{i}(:,j+1)*lambda1^j;
-        test_samples{i}(:,j+1) = test_samples{i}(:,j+1)*lambda1^j;
-        predict_samples{i}(:,j+1) = predict_samples{i}(:,j+1)*lambda1^j;
-    end
+    training_sample_selector{i} = zeros(order+1,n_train);
+    idx = training_indices(:,i);
+    idx = idx + (order+1)*(0:n_train-1);
+    training_sample_selector{i}(idx) = 1;
+    training_sample_selector{i} = training_sample_selector{i}';
+
+    test_sample_selector{i} = zeros(order+1,n_samples-n_train);
+    idx = test_indices(:,i);
+    idx = idx + (order+1)*(0:n_samples-n_train-1);
+    test_sample_selector{i}(idx) = 1;
+    test_sample_selector{i} = training_sample_selector{i}';
 end
 
-
-PC_coefficients = cell(n_b,1);
+%% TT outs estimation
+[~,n_b] = size(b_train);
+TT_outs = cell(n_b,1);
 training_err = zeros(n_b,1);
 test_err = zeros(n_b,1);
 n_iterations = zeros(n_b,1);
 
+
 for i = 1:n_b
-    [x,training_err(i),test_err(i),n_iterations(i)] = f(training_samples,training_out(:,i),x,4,1e-4,2000,test_samples,test_out(:,i),0.1);
+    [y,training_err(i),test_err(i),n_iterations(i)] = f(training_sample_selector,training_samples(:,i),y,4,1e-4,2000,test_sample_selector,test_samples(:,i),0.1);
     [training_err(i) test_err(i) n_iterations(i)] 
-    PC_coefficients{i} = x;
+    TT_outs{i} = y;
 end
 
+%% compute PC coefficients
+PC_coefficients = cell(n_b,1);
+polynomial_roots =  genPolynomialRoots(order+1,polynomial);
+A_r1 = zeros(order+1,order+1);
+for i = 1:order+1
+    A_r1(:,i) = genHermite(polynomial_roots,i-1);
+end
+A_r1_inverse = A_r1^-1;
+for i = 1:n_b
+    PC_coefficients{i} = TT_outs{i};
+    [~,m,r] = TTsizes(x);
+    for j = 1:d
+        Wj = PC_coefficients{i}{j};
+        Wj = reshape(Wj,[r(j),m(j),r(j+1)]);
+        Wj = reshape(permute(Wj, [2 1 3]),m(i),[]);
+        Wj = A_r1_inverse*Wj;
+        Wj = permute(reshape(Wj,m(i),r(i),r(i+1)),[2 1 3]);
+        PC_coefficients{i}{j} = Wj;
+    end
+end
 
-
+%% predict outputs
 [n_predict_samples,~] = size(A_predict);
 b_predict = zeros(n_predict_samples,n_b);
 for i = 1:n_b
--- PC/pc_collocation_tensor_optimization.m
+++ PC/pc_collocation_tensor_optimization.m
@@ -1,8 +1,7 @@
 function [b_predict,PC_coefficients,training_err,test_err] = ...
     pc_collocation_tensor_optimization(A_train,b_train,x,A_predict,order,polynomial,method,...
     left_preconditioning_parameter)
-%PC_COLLOCATION_TENSOR Summary of this function goes here
-%   Detailed explanation goes here
+%x is PC coefficients, A is polynomial samples, b is sample outputs
 switch method
     case "TT-ALS"
         f = @TT_ALS;
@@ -45,7 +44,7 @@ test_err = zeros(n_b,1);
 n_iterations = zeros(n_b,1);
 
 for i = 1:n_b
-    [x,training_err(i),test_err(i),n_iterations(i)] = f(training_samples,training_out(:,i),x,4,1e-4,2000,test_samples,test_out(:,i),lambda2);
+    [x,training_err(i),test_err(i),n_iterations(i)] = f(training_samples,training_out(:,i),x,3,1e-4,2000,test_samples,test_out(:,i),lambda2);
     [training_err(i) test_err(i) n_iterations(i)] 
     PC_coefficients{i} = x;
 end
