a4cb14ca2ee3bdbffb4a6ec41a20b955014e5bdb
williamin18 <ziyuan.wang@mail.mcgill.ca>
2025-06-21 19:02:57 -0400

    20 par samples

--- Tests/TlineNet/TLtest_script.m
+++ Tests/TlineNet/TLtest_script.m
@@ -1,9 +1,9 @@
 clear variables
 
-load('Tests/TlineNet/TlineNet_29par.mat')
+load('Tests/TlineNet/20std_tline_samples.mat')
 
 [n_samples,d] = size(training_samples);
-m = 3;
+m = 4;
 
 % vouts_total = pc_collocation_total(training_samples,vouts_train,samples,m ,'Hermite');
 % norm(vouts_total-vouts)/norm(vouts);
@@ -13,12 +13,13 @@ r = 3;
 x = TTrand(N,r);
 x = TTorthogonalizeLR(x);
 x{d} = x{d}/norm( x{d},'fro');
-% x{1}(1) = vouts_train(1,1);
-% for i = 1:d
-%     x{i}(1)=1;
-% end
+x{1}(1) = vouts_train(1,1);
+for i = 1:d
+    x{i}(1)=1;
+end
 
 tic
- [vouts_TT,PC_coefficients,training_err,test_err,n_iterations] = pc_collocation_tensor_optimization(training_samples,vouts_train,x,samples,m,'Hermite','TT-ALS',0.3,0.2,3);
+ [vouts_TT,PC_coefficients,training_err,test_err,n_iterations] = ...
+     pc_collocation_tensor_optimization(training_samples,vouts_train,x,samples,m,'Hermite','TT-Newton',0.3,0.2,3);
 toc
 norm(vouts_TT-vouts)/norm(vouts)
