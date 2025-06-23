21c9b3d3753c17d25642c1fb0137ccb82850cde5
Ziyuan Wang <ziyuan.wang@mail.mcgill.ca>
2025-06-23 14:39:09 -0400

    .

--- Tests/Filter/Filter_pc_script.m
+++ Tests/Filter/Filter_pc_script.m
@@ -2,13 +2,13 @@ clear variables
 freq = linspace(0.5e9,14e9,101);
 training_data = load('Tests/Filter/data_training.mat');
 mc_data = load('Tests/Filter/data_training.mat');
-S11_train = (abs(training_data.S21));
+S21_train = (abs(training_data.S21));
 training_samples = (training_data.samples);
-S11_mc = (abs(mc_data.S21));
+S21_mc = (abs(mc_data.S21));
 mc_samples = (mc_data.samples);
 
-s = log(S11_train./(1-S11_train));
-s_mc = log(S11_mc./(1-S11_mc));
+s = log(S11_train./(1-S21_train));
+s_mc = log(S21_mc./(1-S21_mc));
 pdf_freq_idx = 80;
 f = figure(5);
 Hmc = histogram( s(:,pdf_freq_idx) ,50,'Normalization','pdf', 'DisplayStyle','bar', 'FaceColor',[0.7 0.7 0.7]);
@@ -20,7 +20,7 @@ m = 4;
 % norm(vouts_total-vouts)/norm(vouts);
 
 N = (m+1)*ones(d,1);
-r = 2;
+r = 3;
 x = TTrand(N,r);
 x = TTorthogonalizeLR(x);
 x{d} = x{d}/norm( x{d},'fro');
@@ -32,5 +32,5 @@ end
 tic
 % [y_out,PC_coefficients] = pc_collocation_total(training_samples,S21_train,mc_data.samples,m,'Legendre');
 [y_out,PC_coefficients,training_err,test_err,n_iterations] = ...
-    pc_collocation_tensor_optimization(training_samples,s,x,mc_samples,m,'Hermite','TT-Newton',0.33,0.2,r);
+    pc_collocation_tensor_optimization(training_samples,s,x,mc_samples,m,'Hermite','TT-Newton',0.3,0.2,r);
 toc
